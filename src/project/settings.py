d989d6247647892b8520a6d7bc769c542e6c2bce
Vicente Reyes <hi@icenreyes.xyz>
2019-05-21 18:27:33 +0800

    added db

--- src/project/settings.py
+++ src/project/settings.py
@@ -86,7 +86,14 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
