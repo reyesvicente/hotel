899fafad6e215d8e24e848d5758a9b77df09e28e
Vicente Reyes <hi@icenreyes.xyz>
2019-10-28 16:40:04 +0800

    Update settings.py
    debug fix

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG')
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
