04e89e8d49278489abe4bc30c9e121be709c8769
Vicente Reyes <hi@icenreyes.xyz>
2019-10-28 16:46:21 +0800

    debug false1
    debug false

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['icvn-hotel.herokuapp.com', '0.0.0.0']
 
