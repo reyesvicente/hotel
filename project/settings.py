b04bc220e508288abe8ae5cbdd41a8f8187d6fbc
Vicente Reyes <hi@icenreyes.xyz>
2019-10-28 17:00:06 +0800

    Update settings.py
    new static settings

--- project/settings.py
+++ project/settings.py
@@ -124,8 +124,13 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
